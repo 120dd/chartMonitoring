b792c4bc9e1ae8383b6f513062673cdf715b2494
Chris.c <cjs120dd@gmail.com>
2022-01-12 15:52:35 +0900

    리펙토링전백업

--- monitoring/monitoring.js
+++ monitoring/monitoring.js
@@ -1,3 +1,5 @@
+import {sendMsg} from './modules/telegram.js';
+
 const userInput = document.querySelectorAll(".userInput");
 console.log(userInput.length);
 userInput[0].onclick = function(){console.log("ih")};
@@ -6,10 +8,32 @@ for (let i = 0; i < userInput.length; i++){
     userInput[i].onfocus = clearValue;
 }
 
-import {sendMsg} from './modules/telegram.js';
+const startBtn = document.querySelector("#startBtn");
+let beforeTime = document.querySelector("#beforeTime");
 
+let sendTeleId = document.querySelector("#teleId").value;
+document.querySelector("#teleId").onchange = function () {
+    sendTeleId = document.querySelector("#teleId").value;
+}
+
+let intervalForMsg;
+document.querySelector("#tradeTimeCheckInterval").onchange = function () {
+    intervalForMsg = document.querySelector("#tradeTimeCheckInterval").value;
+}
+
+/**
+ * this의 value값을 초기화시키는 함수
+ */
+function clearValue() {
+    this.value = "";
+    console.log("초기화");
+}
+/**
+ *특정 url에서 특정 메세지가 올때마다 콜백함수를 실행
+ * @param callback {function} 콜백시 실핼될 함수 넣기
+ */
 function subscribePriceChanged(callback) {
-    const regex = new RegExp(/"code":"PANDO"/);//정규표현식으로 문자를 다루기위해 객채의 생성자함수 호출
+    const regex = new RegExp(/"code":"PANDO"/);//정규표현식사용
     const ws = new WebSocket('wss://nodes.cashierest.com/socket.io/?EIO=4&transport=websocket');
     ws.onmessage = function (event) {
         if (event.data.startsWith('0')) {
@@ -17,13 +41,16 @@ function subscribePriceChanged(callback) {
         } else if (event.data.startsWith('40')) {
             ws.send('42["s",{"join":{"market":"usdt","coin":"2137"},"leave":{}}]');
         } else {
-            if (regex.test(event.data)) {//대응되는 문자열이있는지 검사, 불리언 타입으로 if작동
+            if (regex.test(event.data)) {//대응되는 문자열이있는지 검사 불리언으로 출력
                 callback(event.data);
             }
         }
     }
-}// cashierest 의 Pando 가격이 변경이 되었을때마다 입력하는 callback 함수가 실행되는 함수
+}
 
+/**
+ * 함수 실행시 시간을 특정 태그에 입력하는 함수
+ */
 subscribePriceChanged(function () {
     let lastTime = new Date();
     console.log(lastTime);
@@ -35,7 +62,6 @@ subscribePriceChanged(function () {
 subscribePriceChanged(function (data) {
     convertToObj(data);
 })
-
 /**
  * 알맞지않은 않은 json형태의 스트링을 알맞은 형태의 객체로 변환
  * @param rawMessage {string} 알맞지않은 json형태
@@ -49,17 +75,6 @@ function convertToObj(rawMessage) {
     document.querySelector("#currentTradePrice").value = correctObj;
 }
 
-// /**
-//  * 매게변수의 벨류값을 없애주는 함수
-//  * @param obj {object} 온포커스시 실행되는 함수로 매게변수에 this를 활용하기
-//  */
-// function clearValue(obj) {
-//     console.log("초기화");
-//     obj.value = ""
-// }//입력칸 누르면 기본값 없애주는 함수
-
-const startBtn = document.querySelector("#startBtn");
-let beforeTime = document.querySelector("#beforeTime");
 
 const intervalByMs = () => {
     const intervalTimeMinute = document.querySelector("#tradeTimeCheckInterval").value;
@@ -74,6 +89,9 @@ startBtn.onclick = function () {
     }
 }
 
+/**
+ *
+ */
 function mornitoringStart() {
     beforeTime.value = document.querySelector("#currentTradeTime").value;
     if (document.querySelector("#tradeTimeCheckInterval").value.length < 4) {
@@ -83,7 +101,9 @@ function mornitoringStart() {
         alert("모니터링 간격을 수정해주세요");
     }
 }// 버튼 누르면 실행되는 함수, 확인 시간에 현재 시간 할당, 모니터링 간격 안쓰면 경보
-
+/**
+ *
+ */
 function compareTime() {
     if (beforeTime.value === document.querySelector("#currentTradeTime").value) {
         document.querySelector("#warningConfirmation").checked = true;
@@ -96,6 +116,9 @@ const warningCheckBox = document.querySelector("#warningConfirmation");
 
 const intervalId = setInterval(checkedCheck, 1000)
 
+/**
+ * 전송 확인용 체크박스가 체크되면 동적으로(?) msg변수를 생성, 체크드체크를 정지
+ */
 function checkedCheck() {
     if (warningCheckBox.checked === true) {
         let msg = sendTeleId + ". 님 " + intervalForMsg + "분간 거래가 없었습니다.";
@@ -105,19 +128,4 @@ function checkedCheck() {
     } else {
         console.log("체크안됨");
     }
-}
-
-let sendTeleId = "";
-document.querySelector("#teleId").onchange = function () {
-    sendTeleId = document.querySelector("#teleId").value;
-}
-
-let intervalForMsg;
-document.querySelector("#tradeTimeCheckInterval").onchange = function () {
-    intervalForMsg = document.querySelector("#tradeTimeCheckInterval").value;
-}
-
-export function clearValue() {
-    this.value = "";
-    console.log("초기화");
 }
